d739341ec9269fd357504ca83eb7ba01bf55013e
Junyu Li <junyul24@uci.edu>
2023-03-03 17:42:07 -0800

    Return to the previous version.

--- OpenWeather.py
+++ OpenWeather.py
@@ -10,52 +10,29 @@
 # 86676906
 import urllib
 import json
-import http.client
 from urllib import request, error
 
 
 class OpenWeather:
-    zipcode: str
-    ccode: str
-    apikey: str
-    temperature: int
-    high_temperature: int
-    low_temperature: int
-    longitude: int
-    latitude: int
-    description: str
-    humidity: int
-    sunset: int
-    city: str
 
     def __init__(self, zipcode, ccode):
-        try:
-            assert type(zipcode) is str
-            assert type(ccode) is str
-            assert len(zipcode) > 0
-            assert len(ccode) > 0
-            if zipcode.isspace():
-                raise AssertionError
-            if ccode.isspace():
-                raise AssertionError
-        except AssertionError:
-            print('Invalid zipcode and ccode.')
-        else:
-            self.zipcode = zipcode
-            self.ccode = ccode
+        self.zipcode = zipcode
+        self.ccode = ccode
+        self.apikey = ''
+        self.temperature = 0
+        self.high_temperature = 0
+        self.low_temperature = 0
+        self.longitude = 0
+        self.latitude = 0
+        self.description = ''
+        self.humidity = 0
+        self.sunset = 0
+        self.city = ''
 
     def set_apikey(self, apikey: str) -> None:
         """Set apikey"""
         # TODO: assign apikey value to a class data attribute that can be accessed by class members
-        try:
-            assert type(apikey) is str
-            assert len(apikey) > 0
-            if apikey.isspace():
-                raise AssertionError
-        except AssertionError:
-            print('Invalid apikey')
-        else:
-            self.apikey = apikey
+        self.apikey = apikey
 
     def load_data(self) -> None:
         '''
@@ -65,7 +42,10 @@ class OpenWeather:
         # TODO: assign the necessary response data to the required class data attributes
         try:
             url = f"http://api.openweathermap.org/data/2.5/weather?zip={self.zipcode},{self.ccode}&appid={self.apikey}"
-            json_obj = _download_url(url)
+            json_obj = self._download_url(url)
+        except json.JSONDecodeError:
+            print("Json cannot be decoded.")
+        else:
             self.temperature = json_obj['main']['temp']
             self.high_temperature = json_obj['main']['temp_max']
             self.low_temperature = json_obj['main']['temp_min']
@@ -75,64 +55,26 @@ class OpenWeather:
             self.humidity = json_obj['main']['humidity']
             self.sunset = json_obj['sys']['sunset']
             self.city = json_obj['name']
-        except json.JSONDecodeError:
-            print("Json cannot be decoded.")
-        except InternetError:
-            print('Loss of local connection to the Internet')
-        except InvalidAPIkeyError:
-            print('Your APIkey is not correct.')
-        except APInotFoundError:
+
+    def _download_url(self, url_to_download: str) -> dict:  # 这里处理Invalid data formatting from the remote API
+        response = None
+        r_obj = None
+        try:
+            response = urllib.request.urlopen(url_to_download)
+            json_results = response.read()
+            r_obj = json.loads(json_results)
+        except urllib.error.HTTPError as e:
             print('Failed to download contents of URL')
-        except ServerUnavailable:
-            print('Remote API is unavailable')
-        except InvalidAPIformatError:
+            print('Status code: {}'.format(e.code))
+            print('The remote API is unavailable')
+        except urllib.error.URLError:
+            print('Loss of local connection to the Internet')
+        except json.JSONDecodeError:
             print('Invalid data formatting from the remote API')
-
-
-class InternetError(Exception):
-    pass
-
-
-class InvalidAPIkeyError(Exception):
-    pass
-
-
-class APInotFoundError(Exception):
-    pass
-
-
-class ServerUnavailable(Exception):
-    pass
-
-
-class InvalidAPIformatError(Exception):
-    pass
-
-
-def _download_url(url_to_download: str) -> dict:  # 这里处理Invalid data formatting from the remote API
-    response = None
-    r_obj = None
-    try:
-        response = urllib.request.urlopen(url_to_download)
-        json_results = response.read()
-        r_obj = json.loads(json_results)
-    except urllib.error.HTTPError as e:
-        # print('Failed to download contents of URL')
-        # print('Status code: {}'.format(e.code))
-        if e.code == 404:
-            raise APInotFoundError
-        if e.code == 503:
-            raise ServerUnavailable
-    except urllib.error.URLError:
-        raise InternetError
-    except http.client.InvalidURL:
-        raise InvalidAPIkeyError
-    except ValueError:
-        raise InvalidAPIformatError
-    finally:
-        if response is not None:
-            response.close()
-    return r_obj
+        finally:
+            if response is not None:
+                response.close()
+        return r_obj
 
 
 def main() -> None:
@@ -140,8 +82,8 @@ def main() -> None:
     ccode = "US"
     apikey = "03657b48a28c90947a8068f1f2608dfc"
     url = f"http://api.openweathermap.org/data/2.5/weather?zip={zip},{ccode}&appid={apikey}"
-
-    weather_obj = _download_url(url)
+    obj = OpenWeather(zip, ccode)
+    weather_obj = obj._download_url(url)
     if weather_obj is not None:
         print(weather_obj)
         print(weather_obj['weather'][0]['description'])
--- WebAPI.py
+++ WebAPI.py
@@ -11,20 +11,62 @@
 
 from abc import ABC, abstractmethod
 
+
 class WebAPI(ABC):
 
-  def _download_url(self, url: str) -> dict:
-    #TODO: Implement web api request code in a way that supports
-    # all types of web APIs
-    pass
+    def _download_url(self, url: str) -> dict:
+        # TODO: Implement web api request code in a way that supports
+        # all types of web APIs
+        response = None
+        r_obj = None
+        try:
+          response = urllib.request.urlopen(url_to_download)
+          json_results = response.read()
+          r_obj = json.loads(json_results)
+        except urllib.error.HTTPError as e:
+          print('Failed to download contents of URL')
+          print('Status code: {}'.format(e.code))
+        except urllib.error.URLError:
+          print('Loss of local connection to the Internet')
+        except http.client.InvalidURL:
+          raise InvalidAPIkeyError
+        except ValueError:
+          raise InvalidAPIformatError
+        finally:
+          if response is not None:
+            response.close()
+        return r_obj
 
-  def set_apikey(self, apikey:str) -> None:
-    pass
+    def set_apikey(self, apikey: str) -> None:
+        self.apikey = apikey
 
-  @abstractmethod
-  def load_data(self):
-    pass
+    @abstractmethod
+    def load_data(self):
+      """Calls the web api using the required values and stores the response in class data attributes."""
+      # TODO: assign the necessary response data to the required class data attributes
+      try:
+        url = f"http://api.openweathermap.org/data/2.5/weather?zip={self.zipcode},{self.ccode}&appid={self.apikey}"
+        json_obj = _download_url(url)
+        self.temperature = json_obj['main']['temp']
+        self.high_temperature = json_obj['main']['temp_max']
+        self.low_temperature = json_obj['main']['temp_min']
+        self.longitude = json_obj['coord']['lon']
+        self.latitude = json_obj['coord']['lat']
+        self.description = json_obj['weather'][0]['description']
+        self.humidity = json_obj['main']['humidity']
+        self.sunset = json_obj['sys']['sunset']
+        self.city = json_obj['name']
+      except json.JSONDecodeError:
+        print("Json cannot be decoded.")
+      except InternetError:
+        print('Loss of local connection to the Internet')
+      except InvalidAPIkeyError:
+        print('Your APIkey is not correct.')
+      except APInotFoundError:
+        pass
+      except InvalidAPIformatError:
+        print('Invalid data formatting from the remote API')
 
-  @abstractmethod
-  def transclude(self, message:str) -> str:
-    pass
+    @abstractmethod
+    def transclude(self, message: str) -> str:
+        pass
